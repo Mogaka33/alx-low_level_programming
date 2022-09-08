bf2c1ddfdb6889b547252bf7d311dd68441c72cb
Mogaka33 <gmogaka33@gmail.com>
2022-09-08 12:55:24 -0700

    linux

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 /*
  * File: 101-quote.c
  * Auth: mogaka33
- */
+*/
 
 #include <unistd.h>
 
@@ -10,12 +10,12 @@
  *                followed by a new line, to standard error.
  *
  * Return: Always 1.
- */
+*/
 int main(void)
 {
-  write(2,
+write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	59);
+59);
 
-  return (1);
+ return (1);
 }

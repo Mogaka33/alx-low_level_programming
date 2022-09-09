202bad1527442994a01188540092314008d45d2e
Mogaka33 <gmogaka33@gmail.com>
2022-09-09 14:02:13 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,6 @@
 
 /**
  * main - Prints all single digit numbers of base 10 starting from 0,
- *        only 
-/*
- * File: 6-print_numberz.c
- * Auth: Mogaka33
- */
-
-#include <stdio.h>
-
-/**
- * main - Prints all single digit numbers of base 10 starting from 0,
  *        only using putchar and without char variables.
  *
  * Return: Always 0.
